9d6f20c559b19edd2e51a539ee3988fbca43a501
Dave <dlp140@gmail.com>
2022-07-27 15:12:31 -0400

    basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-'use strict';
+'use strict'
+/*
+console.log(document.querySelector(".message").textContent)
+document.querySelector(".message").textContent = "Correct Number!"
+
+document.querySelector(".number").textContent = 13
+document.querySelector(".score").textContent = 10
+
+document.querySelector(".guess").value = 23
+console.log(document.querySelector(".guess").value)
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+document.querySelector('.number').textContent = secretNumber
+
+let score = 20
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value)
+  console.log(guess, typeof guess)
+
+  // No guess
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!'
+  }
+
+  // Player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+  }
+
+  // Guess too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!'
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!'
+      document.querySelector('.score').textContent = 0
+    }
+  }
+
+  // Guess too low
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!'
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!'
+      document.querySelector('.score').textContent = 0
+    }
+  }
+})
