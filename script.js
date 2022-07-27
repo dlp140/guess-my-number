afba55c0d07340a32da78f18d3b0b6975db48d76
Dave <dlp140@gmail.com>
2022-07-27 15:27:47 -0400

    replay functionaliy added

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ document.querySelector(".guess").value = 23
 console.log(document.querySelector(".guess").value)
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
-document.querySelector('.number').textContent = secretNumber
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 
 let score = 20
 
@@ -27,6 +26,7 @@ document.querySelector('.check').addEventListener('click', () => {
   // Player wins
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!'
+    document.querySelector('.number').textContent = secretNumber
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
   }
@@ -55,3 +55,26 @@ document.querySelector('.check').addEventListener('click', () => {
     }
   }
 })
+
+document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1
+  score = 20
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('.score').textContent = score
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.number').style.width = '15rem'
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.guess').value = ''
+})
+
+// JavaScript in the Browser: DOM and Events
+// Coding Challenge #1
+// Implement a game rest functionality, so that the player can make a new guess!
+// Your tasks:
+// 1. Select the element with the 'again' class and attach a click event handler
+// 2. In the handler function, restore initial values of the 'score' and
+// 'secretNumber' variables
+// 3. Restore the initial conditions of the message, number, score and guess input
+// fields
+// 4. Also restore the original background color (#222) and number width (15rem)
+// GOOD LUCK 😀
